f2c46adc2fdf002cde828172680b3482ef4fd3bd
Shreyansh Shukla <bugbreaker18@gmail.com>
2024-06-12 04:49:32 +0530

    connect to mongodb

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// require('dotenv').config({path: "./env"})
+import dotenv from "dotenv"
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
