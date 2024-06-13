5386faf6a646d4e6eec0c62244ef304212827fb4
Shreyansh Shukla <bugbreaker18@gmail.com>
2024-06-13 06:48:32 +0530

    Added user controller (register) and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,84 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/ApiError.js'
+import {User} from '../models/user.model.js'
+import {uploadOnCloudinary} from '../utils/cloudinary.js'
+import { ApiRresponse } from '../utils/ApiResponse.js';
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json(
+    
+    // 0. get user data from frontend
+    // 1. middleware = for input validation (check if empty, check if user already exists (check by username and email))
+    // check for images (avatar)
+    // upload them to cloudinary - verify that images are finally uploaded
+
+    // 2. create user object and push to db (db.create())
+    // Do not pass password and refreshToken to the user = remove them from the response
+    // check for user creation (if it is created successfullly)
+    // 3. return response
+
+    const { fullName, email, username, password } = req.body
+    console.log(email);
+
+    // if (fullName === "") {
+    //     throw new ApiError(400, "Name is required")
+    // }
+
+    if (
+        [fullName, email, username, password].some( (fields) => { return fields?.trim === ""})
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne(
         {
-            msg: "ok"
+            $or: [
+                    { username }, 
+                    { email }
+            ]
         }
     )
+    console.log(existedUser);
+    if (existedUser) {
+        throw new ApiError(409, "User with that credentials already exists")
+    }
+
+    console.log(req.files?.avatar);
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar needed ")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar needed ")
+    }
+
+    const user = await User.create(
+        {
+            fullName, 
+            avatar: avatar.url,
+            email, 
+            password, 
+            coverImage: coverImage?.url || "",
+            username: username.toLowerCase()
+        }
+    )
+
+    const createdUser = await User.findById(user._id)
+    .select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(200).json(
+        new ApiRresponse(200, createdUser, "User registered successfully")
+    )
+
 })
 
 export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -37,7 +37,7 @@ const userSchema = new Schema (
         watchHistory: [
             {
                 type: mongoose.Schema.Types.ObjectId,
-                ref: Video
+                ref: "Video"
             }
         ], 
         avatar: {
@@ -58,7 +58,7 @@ const userSchema = new Schema (
 userSchema.pre("save", async function (next) {
     
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
