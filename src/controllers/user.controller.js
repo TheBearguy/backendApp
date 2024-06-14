8eb5624443dd3dcccf00df642a76a8fd3cb3f36e
Shreyansh Shukla <bugbreaker18@gmail.com>
2024-06-14 13:27:40 +0530

    Now we can get the user channel page

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -396,6 +396,92 @@ const updateUserCoverImage = asyncHandler(async (req, res)  => {
 
 })
 
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const {username} = req.params
+
+    if (!username) {
+        throw new ApiError(
+            403, 
+            "Username not found"
+        )
+    }
+
+    const channel = User.aggregate([
+        {
+            $match: {
+                username: username.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                foreignField: "channel",
+                localField: "_id",
+                as: "subscribers"
+            }
+        }, 
+        {
+            $lookup: {
+                from: "subscriptions", 
+                foreignField: subscriber,
+                as: "subscribedTo"
+            }
+        }, 
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                }, 
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                }, 
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id, "$subscribers.subscriber"]
+                        }, 
+                        then: true, 
+                        else: false
+                    }
+                }
+            }
+        }, 
+        {
+            $project: {
+                fullName: 1, 
+                username: 1,
+                subscribers: 1, 
+                subscribedTo: 1, 
+                channelsSubscribedToCount: 1, 
+                isSubscribed: 1, 
+                avatar: 1, 
+                coverImage: 1, 
+                email: 1, 
+                createdAt: 1
+            }
+        }
+    ])
+
+})
+
+if (!channel?.length) {
+    throw new ApiError(
+        400, 
+        "Channel does not exists"
+    )
+}
+
+return res.status(200).json(
+    new ApiRresponse(
+        200, 
+        channel[0], 
+        "User channel fetched successfully"
+    )
+)
+
+console.log(channel);
+
 export {
     registerUser,
     loginUser, 
