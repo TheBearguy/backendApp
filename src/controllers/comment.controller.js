22196bef20bddc2aef9a6ee9a17820d695944ad8
Shreyansh Shukla <bugbreaker18@gmail.com>
2024-06-18 15:48:43 +0530

    comments routes and controller done. Can comment now

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,7 +3,7 @@ import { ApiRresponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import mongoose from "mongoose"
 import { loginUser } from "./user.controller.js"
-
+import { Comment } from "../models/comment.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     // TODO : get all ccomments on a video
@@ -56,8 +56,114 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 })
 
+const addComments = asyncHandler(async (req, res) => {
+    const {channelId, videoId} = req.params;
+    const {content} = req.body;
+
+    if (!videoId && !channelId) {
+        throw new ApiError(
+            400, 
+            "videoId or ChannelId not found to add any comment"
+        )
+    }
+
+    const comment = await Comment.create(
+        {
+            content, 
+            video: videoId, 
+            owner: channelId
+        }
+    )
+
+    if (!comment) {
+        throw new ApiError(
+            400, 
+            "Error occured in creating the comment"
+        )
+    }
+    
+    return res.status(200).json(
+        new ApiRresponse(
+            200, 
+            "Comment added successfully"
+        )
+    )
+
+})
+
+const deleteComment = asyncHandler(async (req, res) => {
+    const {commentId} = req.params;
+
+    if (!commentId) {
+        throw new ApiError(
+            400, 
+            "no commentId found"
+        )
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    if (!deletedComment) {
+        throw new ApiError(
+            400, 
+            "Comment is not yet deleted"
+        )
+    }
+
+    return res.status(200)
+    .json(
+        new ApiRresponse(
+            200, 
+            "Comment Deleted"
+        )
+    )
+
+})
+
+
+const updateComment = asyncHandler(async (req, res) => {
+    const { commentId } = req.params;
+    const { content } = req.body;
+    
+    if (!commentId && !content) {
+        throw new ApiError(
+            400, 
+            "commentId or Content to be updated not found"
+        )
+    }
+
+    const updatedComment = Comment.findByIdAndUpdate({
+        commentId
+        },
+        {
+            $set: {
+                content: content
+            }
+        }, 
+        {new: true}
+    )
+
+    if (!updatedComment) {
+        throw new ApiError(
+            400, 
+            "Comment updation failed"
+        )
+    }
+    
+    return res.status(200)
+    .json(
+        new ApiRresponse(
+            200, 
+            "Comment updpated successfully"
+        )
+    )
+
+})
 
 
 export {
-    getVideoComments
+    getVideoComments, 
+    addComments, 
+    deleteComment, 
+    updateComment
 }
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { getVideoComments } from "../controllers/comment.controller.js";
-
+import { addComments, deleteComment, getVideoComments, updateComment } from "../controllers/comment.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // Call controllers here to perform the task for a particular method request.
 
-router.route("/vid-comments/:videoId").get(getVideoComments);
+router.route("/vid-comments/:videoId").get(verifyJWT, getVideoComments);
+router.route("/create/:channelId/:videoId").post(verifyJWT, addComments);
+router.route("/delete-comments/:commentId").post(verifyJWT, deleteComment);
+router.route("/update-comments/commentId").patch(verifyJWT, updateComment)
 
 export default router;      
