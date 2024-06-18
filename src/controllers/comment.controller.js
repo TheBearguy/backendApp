6e5b119b0e90fefb48e11b2f6367ba5c63b88ef7
Shreyansh Shukla <bugbreaker18@gmail.com>
2024-06-18 05:40:17 +0530

    comment boilerplate

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -6,8 +6,35 @@ import mongoose from "mongoose"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     // TODO : get all ccomments on a video
-
+    const { videoId } = req.params
+    if (!videoId) {
+        throw new ApiError(400, "Invalid videoId")
+    }
     
+    const video = await Video.findById(
+        videoId
+    )
+
+    if (!video) {
+        throw new ApiError(400, "Video not found")
+    }
+
+    const comments = Comment.findById({
+        videoId: videoId
+    })
+
+    if (!comments) {
+        throw new ApiError(400, "No comments found for this video")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiRresponse(
+            200, 
+            comments, 
+            "Comments fetched successfully"
+        )
+    )
 
 })
 
---
+++ src/routes/comment.routes.js
@@ -0,0 +1,10 @@
+// import { Router } from "express";
+
+// const router = Router();
+
+// // Call controllers here to perform the task for a particular method request.
+
+// router.route("/vid-comments/:videoId").get(getVideoComments)
+
+
+// export default router;
