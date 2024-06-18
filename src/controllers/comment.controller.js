668d4538976125874a345a030ccff1a2cfcc8ab4
Shreyansh Shukla <bugbreaker18@gmail.com>
2024-06-18 14:14:39 +0530

    comments can be viewed

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -2,36 +2,54 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiRresponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import mongoose from "mongoose"
+import { loginUser } from "./user.controller.js"
 
 
 const getVideoComments = asyncHandler(async (req, res) => {
     // TODO : get all ccomments on a video
     const { videoId } = req.params
+
+    console.log("Video ID is : ", videoID);
+
     if (!videoId) {
         throw new ApiError(400, "Invalid videoId")
     }
     
-    const video = await Video.findById(
-        videoId
-    )
-
-    if (!video) {
-        throw new ApiError(400, "Video not found")
-    }
-
-    const comments = Comment.findById({
-        videoId: videoId
-    })
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        }, 
+        {
+            $lookup: {
+                from: "users", 
+                localfield: "owner", 
+                foreignField: "_id", 
+                as: "owner", 
+                pipeline: [
+                    {
+                        $project: {
+                        fullName: 1, 
+                        username: 1, 
+                        avatar: 1
+                        }
+                    }
+                ]
+            }
+        }
+    ])
 
     if (!comments) {
-        throw new ApiError(400, "No comments found for this video")
+        throw new ApiError(
+            400, 
+            "No comments found on this video"
+        )
     }
 
-    return res.status(200)
-    .json(
+    return res.status(200).json(
         new ApiRresponse(
             200, 
-            comments, 
             "Comments fetched successfully"
         )
     )
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,10 +1,10 @@
-// import { Router } from "express";
+import { Router } from "express";
+import { getVideoComments } from "../controllers/comment.controller.js";
 
-// const router = Router();
+const router = Router();
 
-// // Call controllers here to perform the task for a particular method request.
+// Call controllers here to perform the task for a particular method request.
 
-// router.route("/vid-comments/:videoId").get(getVideoComments)
+router.route("/vid-comments/:videoId").get(getVideoComments);
 
-
-// export default router;
+export default router;      
